cb4c30ec7c0ec11fb2a3e0275a7c943be0fb5f3d
kennCK <kennettecanales@gmail.com>
2017-08-07 18:20:22 +0800

    update

--- api/app/Ilinya/Database/DBManager.php
+++ api/app/Ilinya/Database/DBManager.php
@@ -4,7 +4,7 @@ namespace App\Ilinya\Database;
 
 use App\TempCustomFieldsStorage as DBFields;
 use App\QueueFormField as FormFields;
-
+use App\BotStatusTracker;
 
 class DBManager{
   public static function get($queueFormId){
@@ -19,5 +19,17 @@ class DBManager{
     );
 
     return DBFields::save($data);
+  }
+
+  public static function saveStatus($userId, $status){
+    $tracker = new BotStatusTracker();
+    $tracker->facebook_id = $userId;
+    $tracker->status = $status;
+    $tracker->save();
+  }
+
+  public static function updateStatus($userId, $status){
+    $data = array("status" => $status);
+    $tracker = BotStatusTracker::where('facebook_id', $userId)->update($data);
   }
 }
