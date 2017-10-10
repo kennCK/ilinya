b85d81a92cadbf57e6f4a8e63c86c8609edd3f1d
kennCK <kennettecanales@gmail.com>
2017-10-10 10:07:19 +0800

    live

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let LIVE_BACKEND_URL = 'https://app.ilinya.com/api' // 'http://johnenrick.com'
 let DEV_BACKEND_URL = 'http://localhost/ilinya/api'
-let isDev = true // Set to false in live
+let isDev = false // Set to false in live
 let BACKEND_URL = isDev ? DEV_BACKEND_URL : LIVE_BACKEND_URL
 export default{
   IS_DEV: isDev,
