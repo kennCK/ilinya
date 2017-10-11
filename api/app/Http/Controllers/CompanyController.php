500aeaf7d51e2fa2df55cf570a485fba73de72d4
kennCK <kennettecanales@gmail.com>
2017-10-11 16:24:56 +0800

    update registration

--- api/app/Http/Controllers/CompanyController.php
+++ api/app/Http/Controllers/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Company;
 
 class CompanyController extends APIController
@@ -19,13 +20,21 @@ class CompanyController extends APIController
       "lat",
       "lng"
     );
+
+    $this->editableForeignTable = array(
+      'accounts'
+    );
+    $this->foreignTable = array(
+      'accounts'
+    );
   }
 
   public function create(Request $request){
      $request = $request->all();
-     $request['username'] = $request['accounts.email'];
-     $request['password'] = Hash::make($request['accounts.password']);
-     //$this->createEntry($request);
-     //return $this->output();
+     $request['accounts']['username'] = $request['accounts']['email'];
+     $request['accounts']['password'] = Hash::make($request['accounts']['password']);
+     echo json_encode($request);
+     $this->createEntry($request);
+     return $this->output();
   }
 }
--- src/modules/home/CompanyRegistration.vue
+++ src/modules/home/CompanyRegistration.vue
@@ -11,7 +11,7 @@
         </span>
          <form ref="registration">
           <input-group
-            :inputs="inputs" :form_status="'create'" :error_list="errorList"
+            :inputs="inputs" :form_status="'create'" :error_list="errorList" v-on:form_data_changed="valueChange"
           ></input-group>
         </form>
           <button class="btn btn-primary pull-right" v-on:click="register()">Next</button>
@@ -20,6 +20,7 @@
 </template>
 <script>
 import ROUTER from '../../router'
+import AUTH from '../../services/auth'
 export default {
   name: '',
   components: {
@@ -33,6 +34,7 @@ export default {
         business_type_id: {
           label_colspan: 0,
           input_type: 'select',
+          db_name: 'company[business_type_id]',
           input_setting: {
             option_function: (instance) => {
               let parameter = {
@@ -59,48 +61,93 @@ export default {
             }
           }
         },
+        username: {
+          label_colspan: 0,
+          db_name: 'username',
+          placeholder: 'Username'
+        },
         email: {
           label_colspan: 0,
-          db_name: 'accounts[email]',
+          db_name: 'email',
           placeholder: 'Email Address'
         },
         password: {
           label_colspan: 0,
-          db_name: 'accounts[password]',
-          placeholder: 'Password'
+          db_name: 'password',
+          placeholder: 'Password',
+          input_type: 'password'
         },
         confirm_password: {
           label_colspan: 0,
-          placeholder: 'Confirm Password'
+          placeholder: 'Confirm Password',
+          input_type: 'password'
         },
         name: {
           label_colspan: 0,
-          db_name: 'name',
-          placeholder: 'Company Name'
+          db_name: 'company[name]',
+          placeholder: 'Company or Organization Name'
         },
         address: {
           label_colspan: 0,
-          db_name: 'address',
+          db_name: 'company[address]',
           placeholder: 'Address'
         }
       },
       errorList: {},
-      errorStatus: ''
+      errorStatus: '',
+      myData: {},
+      passwordFlag: false,
+      usernameFlag: false
     }
   },
   methods: {
-    register(){
-      this.APIFormRequest('company/create', this.$refs.registration, response => {
-        if(response.error.status === 100){
-          this.errorStatus = ('email' in response.error.message && !('name' in response.error.message)) ? 'Email Address already taken.' : 'Please fill up the required informations.'
-          console.log(response.error.message['name'])
-        }else if(response.error.status !== 'undefined'){
-          this.redirect()
-        }
-      })
+    valueChange(fieldName, value){
+      if(value !== null){
+        this.myData[fieldName] = value
+        this.validate()
+      }
     },
-    redirect(){
-      ROUTER.push('/')
+    validate(){
+      if('password' in this.myData || 'confirm_password' in this.myData){
+        console.log('validate')
+        if(this.myData.password.localeCompare(this.myData.confirm_password) === 0){
+          this.errorStatus = ''
+          this.passwordFlag = true
+        }else{
+          this.errorStatus = 'Please confirm your password.'
+          this.passwordFlag = false
+        }
+      }
+      if('username' in this.myData){
+        if(this.myData.username.length < 10){
+          this.errorStatus = 'Username must be atleast 10 characters.'
+          this.usernameFlag = false
+        }else{
+          this.errorStatus = ''
+          this.usernameFlag = true
+        }
+
+      }
+    },
+    register(){
+      if(this.passwordFlag === true && this.usernameFlag === true){
+        this.APIFormRequest('account/create', this.$refs.registration, response => {
+          if(response.error.status === 100){
+            this.errorStatus = ('email' in response.error.message && !('name' in response.error.message)) ? 'Email Address already taken.' : 'Please fill up the required informations.'
+          }else if(response.error.status !== 'undefined'){
+            this.login()
+          }
+        })
+      }else{
+        console.log('error')
+      }
+    },
+    login(){
+      AUTH.authenticate(this.myData.username, this.myData.password, (response) => {
+        ROUTER.push('dashboard')
+      }, (response, status) => {
+        this.errorStatus = (status === 401) ? 'Your Username and password didnot matched.' : 'Cannot log in? Contact us through email: support@ilinya.com'
+      })
     }
   }
 }
