7ace1b6c13ba3dff1250d82ffc312047a5e70efd
kennCK <kennettecanales@gmail.com>
2017-10-16 10:14:28 +0800

    update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-let LIVE_BACKEND_URL = 'https://devapi.ilinya.com/api' // DEV
-// let LIVE_BACKEND_URL = 'https://api.ilinya.com/api' //PRODUCTION
+// let LIVE_BACKEND_URL = 'https://devapi.ilinya.com/api' // DEV
+let LIVE_BACKEND_URL = 'https://api.ilinya.com/api' // PRODUCTION
 let DEV_BACKEND_URL = 'http://localhost/ilinya/api'
-let isDev = true // Set to false in live
+let isDev = false // Set to false in live
 let BACKEND_URL = isDev ? DEV_BACKEND_URL : LIVE_BACKEND_URL
 export default{
   IS_DEV: isDev,
