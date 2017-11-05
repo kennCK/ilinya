0cd48c640f8551502e2249cc2d59f4571f83d828
kennCK <kennettecanales@gmail.com>
2017-11-06 06:38:12 +0800

    update

--- api/database/seeds/QueueFormFieldsTableSeeder.php
+++ api/database/seeds/QueueFormFieldsTableSeeder.php
@@ -14,101 +14,69 @@ class QueueFormFieldsTableSeeder extends Seeder
         DB:: table('queue_form_fields') -> insert(array(
 
           /*
-            K1
+            PSYCH
           */
           array(
             "id"    => "1", "queue_form_id" => "1", "is_admin_only" => '0', "sequence" => "1","description"  => "What is your Complete Name?", "type" => "text"
           ),
           array(
-            "id"    => "2", "queue_form_id" => "1", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your Contact Number?", "type" => "number"
+            "id"    => "2", "queue_form_id" => "1", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your ID Number?", "type" => "text"
           ),
           array(
-            "id"    => "3", "queue_form_id" => "1", "is_admin_only" => '0', "sequence" => "3","description"  => "How many are you?", "type" => "number"
+            "id"    => "3", "queue_form_id" => "1", "is_admin_only" => '0', "sequence" => "3","description"  => "What is your Course and Year Level?", "type" => "text"
           ),
           array(
-            "id"    => "4", "queue_form_id" => "1", "is_admin_only" => '0', "sequence" => "4","description"  => "How many hours you want to reserve?", "type" => "number"
+            "id"    => "4", "queue_form_id" => "1", "is_admin_only" => '0', "sequence" => "4","description"  => "What is your Concerns?", "type" => "text"
           ),
 
           /*
-            Roble
+            PSYCH ENROLMENT
           */
           array(
             "id"    => "5", "queue_form_id" => "2", "is_admin_only" => '0', "sequence" => "1","description"  => "What is your Complete Name?", "type" => "text"
           ),
           array(
-            "id"    => "6", "queue_form_id" => "2", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your Contact Number?", "type" => "number"
+            "id"    => "6", "queue_form_id" => "2", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your ID Number?", "type" => "text"
           ),
           array(
-            "id"    => "7", "queue_form_id" => "2", "is_admin_only" => '0', "sequence" => "3","description"  => "What is your Home Address?", "type" => "text"
+            "id"    => "7", "queue_form_id" => "2", "is_admin_only" => '0', "sequence" => "3","description"  => "What is your Course and Year Level?", "type" => "text"
+          ),
+          array(
+            "id"    => "8", "queue_form_id" => "2", "is_admin_only" => '0', "sequence" => "4","description"  => "What is your Concerns?", "type" => "text"
           ),
 
           /*
-            Cinema
+            DCLL
           */
           array(
-            "id"    => "8", "queue_form_id" => "3", "is_admin_only" => '0', "sequence" => "1","description"  => "What is your Complete Name?", "type" => "text"
+            "id"    => "9", "queue_form_id" => "3", "is_admin_only" => '0', "sequence" => "1","description"  => "What is your Complete Name?", "type" => "text"
           ),
           array(
-            "id"    => "9", "queue_form_id" => "3", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your Contact Number?", "type" => "number"
+            "id"    => "10", "queue_form_id" => "3", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your ID Number?", "type" => "text"
           ),
           array(
-            "id"    => "10", "queue_form_id" => "3", "is_admin_only" => '0', "sequence" => "3","description"  => "What is your Email Address?", "type" => "email"
-          ),
-
-           /*
-            Pino
-          */
-          array(
-            "id"    => "11", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "1","description"  => "What is your Complete Name?", "type" => "text"
+            "id"    => "11", "queue_form_id" => "3", "is_admin_only" => '0', "sequence" => "3","description"  => "What is your Course and Year Level?", "type" => "text"
           ),
           array(
-            "id"    => "12", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your Contact Number?", "type" => "number"
-          ),
-          array(
-            "id"    => "13", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "3","description"  => "How many are you?", "type" => "text"
-          ),
-          array(
-            "id"    => "14", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "4","description"  => "When will eat? Lunch or Dinner?", "type" => "text"
-          ),
-
-
-            /*
-            Pino
-          */
-          array(
-            "id"    => "15", "queue_form_id" => "5", "is_admin_only" => '0', "sequence" => "1","description"  => "What is your Complete Name?", "type" => "text"
-          ),
-          array(
-            "id"    => "16", "queue_form_id" => "5", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your ID Number?", "type" => "text"
-          ),
-          array(
-            "id"    => "17", "queue_form_id" => "5", "is_admin_only" => '0', "sequence" => "3","description"  => "How much do you want to pay?", "type" => "number"
-          ),
-          array(
-            "id"    => "18", "queue_form_id" => "5", "is_admin_only" => '0', "sequence" => "4","description"  => "What is the purpose of payment?", "type" => "text"
+            "id"    => "12", "queue_form_id" => "3", "is_admin_only" => '0', "sequence" => "4","description"  => "What is your Concerns?", "type" => "text"
           ),
 
           /*
-            iLinya Survey Form
+            DCLL ENROLMENT
           */
           array(
-            "id"    => "19", "queue_form_id" => "6", "is_admin_only" => '1', "sequence" => "1","description"  => "Are the statements or instructions clear and easily understood?[5 Highest - 1 Lowest]", "type" => "number"
+            "id"    => "13", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "1","description"  => "What is your Complete Name?", "type" => "text"
           ),
           array(
-            "id"    => "20", "queue_form_id" => "6", "is_admin_only" => '1', "sequence" => "2","description"  => "Are the functionalities easy to navigate?[5 Highest - 1 Lowest]", "type" => "number"
+            "id"    => "14", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "2","description"  => "What is your ID Number?", "type" => "text"
           ),
           array(
-            "id"    => "21", "queue_form_id" => "6", "is_admin_only" => '1', "sequence" => "3","description"  => "What do you like most about iLinya?", "type" => "text"
+            "id"    => "15", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "3","description"  => "What is your Course and Year Level?", "type" => "text"
           ),
           array(
-            "id"    => "22", "queue_form_id" => "6", "is_admin_only" => '1', "sequence" => "4","description"  => "What are the issues you encountered by using iLinya?", "type" => "text"
+            "id"    => "16", "queue_form_id" => "4", "is_admin_only" => '0', "sequence" => "4","description"  => "What is your Concerns?", "type" => "text"
           ),
-          array(
-            "id"    => "23", "queue_form_id" => "6", "is_admin_only" => '1', "sequence" => "5","description"  => "What are the areas need to improve about iLinya?", "type" => "text"
-          ),
-          array(
-            "id"    => "24", "queue_form_id" => "6", "is_admin_only" => '1', "sequence" => "6","description"  => "Are you excited to use Ilinya soon?[5 Highest - 1 Lowest]", "type" => "number"
-          ),
+          
         ));
     }
 }
--- api/database/seeds/QueueFormsTableSeeder.php
+++ api/database/seeds/QueueFormsTableSeeder.php
@@ -14,52 +14,36 @@ class QueueFormsTableSeeder extends Seeder
         DB:: table('queue_forms') -> insert(array(
           array(
             "id"    => "1",
-            "company_id" => "1",
+            "company_id" => "2",
             "availability" => "1",
-            "detail" => "KTV Reservation Form",
-            "title"  => "Reservation",
-            "is_private"  => "0"
+            "detail" => "Chair's Concerns",
+            "title"  => "Enrolment Purposes",
+            "is_private"  => "1"
           ),
           array(
             "id"    => "2",
             "company_id" => "2",
             "availability" => "1",
-            "detail" => "Shippings Ticket from Cebu to Siquijor",
-            "title"  => "Ticket",
-            "is_private"  => "0"
+            "detail" => "Enrolment Concerns",
+            "title"  => "2nd Semester Enrolment",
+            "is_private"  => "1"
           ),
           array(
             "id"    => "3",
             "company_id" => "3",
             "availability" => "1",
-            "detail" => "Movie Ticket",
-            "title"  => "Ticket",
-            "is_private"  => "0"
+            "detail" => "Chair's Concerns",
+            "title"  => "Enrolment Purposes",
+            "is_private"  => "1"
           ),
           array(
             "id"    => "4",
-            "company_id" => "4",
+            "company_id" => "3",
             "availability" => "1",
-            "detail" => "Restaurant Reservation Form",
-            "title"  => "Reservation",
-            "is_private"  => "0"
+            "detail" => "Enrolment Concerns",
+            "title"  => "2nd Semester Enrolment",
+            "is_private"  => "1"
           ),
-          array(
-            "id"    => "5",
-            "company_id" => "5",
-            "availability" => "1",
-            "detail" => "Pay Tuition Fee Form",
-            "title"  => "Payment",
-            "is_private"  => "0"
-          ),
-          array(
-            "id"    => "6",
-            "company_id" => "6",
-            "availability" => "1",
-            "detail" => "Survey Form",
-            "title"  => "Survey",
-            "is_private"  => "0"
-          )
         ));
     }
 }
